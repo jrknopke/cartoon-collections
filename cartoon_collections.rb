3dc77eae1cfef3965cb18594fc258673bb4e3e54
Joseph Knopke <josephknopke@gmail.com>
2020-12-10 20:15:33 -0600

    commit tree 0dd32e86af6e38b857b4ac3826734a047d868b52

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,11 +6,16 @@ def summon_captain_planet(array)
   array.map{|item| "#{item.capitalize}!"}
 end
 
-def long_planeteer_calls()
-  
+def long_planeteer_calls(long_planeteer_calls)
+  answer = false
+  long_planeteer_calls.each{|call|
+  if call.length > 4
+    answer = true
+  end}
+    answer
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find{|cheese| cheese_types.include?(cheese)}
 end
